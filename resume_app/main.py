10220edf6db3778292c6847a8d9dfde977109ad1
Patrick Gray <clifgray@Patricks-MacBook-Pro.local>
2015-06-27 00:31:38 -0400

    adding in templates and changing the app to reflect my normal setup

--- resume_app/main.py
+++ resume_app/main.py
@@ -1,12 +1,51 @@
-import os
-from flask import Flask
+'''
+Created on June 27, 2015
+
+@author: pgray
+'''
+
+######################################################
+################# initial setup ######################
+######################################################
+
+from flask import Flask, redirect, render_template, request
+from flask_pymongo import PyMongo
+from datetime import datetime
+
+from jinja2 import Environment, PackageLoader
+
+import json, subprocess, os, copy
+
+MONGO_URL = os.environ.get('MONGO_URL')
+if not MONGO_URL:
+    MONGO_URL = "mongodb://localhost:27017/resume_app";
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+app.config['MONGO_URI'] = MONGO_URL
+mongo = PyMongo(app)
 
+env = Environment(loader=PackageLoader('resume_app', 'templates'))
+
+######################################################
+################# functions and db work ##############
+######################################################
+
+
+######################################################
+################# web app routes #####################
+######################################################
+
+@app.route('/', methods=['GET'])
+def main():
+	if request.method == "GET":
+		return render_template('main.html')
+	else:
+		pass
+
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    
+
