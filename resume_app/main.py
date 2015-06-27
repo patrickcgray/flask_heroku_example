ce9b1306627252e907c0996d9762386923788401
Patrick Gray <clifgray@Patricks-MacBook-Pro.local>
2015-06-27 00:53:49 -0400

    fixed templates directory and got it running locally

--- resume_app/main.py
+++ resume_app/main.py
@@ -9,7 +9,7 @@ Created on June 27, 2015
 ######################################################
 
 from flask import Flask, redirect, render_template, request
-from flask_pymongo import PyMongo
+from flask.ext.pymongo import PyMongo
 from datetime import datetime
 
 from jinja2 import Environment, PackageLoader
@@ -20,12 +20,12 @@ MONGO_URL = os.environ.get('MONGO_URL')
 if not MONGO_URL:
     MONGO_URL = "mongodb://localhost:27017/resume_app";
 
-app = Flask(__name__)
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+app = Flask(__name__, template_folder=tmpl_dir)
 
 app.config['MONGO_URI'] = MONGO_URL
 mongo = PyMongo(app)
-
-env = Environment(loader=PackageLoader('resume_app', 'templates'))
 
 ######################################################
 ################# functions and db work ##############
@@ -40,12 +40,14 @@ env = Environment(loader=PackageLoader('resume_app', 'templates'))
 def main():
 	if request.method == "GET":
 		return render_template('main.html')
+		#return "Hello, World!"
 	else:
 		pass
 
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
     
 
